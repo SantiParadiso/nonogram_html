9e31a89a76b595832a3233f0d4909a2c38ddc764
hacepan <santiparadise@gmail.com>
2022-11-28 20:22:58 -0300

    general ux flow changes

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-class Nonon { //esta clase guarda todos los datos para mandar a un json
+// FALTA EL BOTON DE "CLEAR"
+
+// TAMBIEN VOY A GUARDAR EL PROGRESO DEL USUARIO EN EL LOCAL STORAGE,
+// ASI PUEDE VOLVER A METERSE LAS VECES QUE QUIERA DESPUES DE TERMINAR SU DIBUJO.
+
+// NECESITA UN ESPACIO DONDE PUEDA COPIAR EL LINK (CAPAZ CON UN BOTÓN) DESPUES
+// DE APRETAR OTRO BOTON DE "DONE!"
+
+
+class Nonon { 
+    //esta clase guarda todos los datos para mandar a un json
+
+    // ESTO ESTABA PENSADO PARA MANDAR POR API, PERO AHORA VOY A RECOLECTAR (POR AHORA) 3 DATOS
+    // UN NUMERO BINARIO QUE CONTENGA TODOS LOS VALORES EN EL TABLERO, QUE VOY A PASAR A DECIMAL O HEXADECIMAL.
+    // LOS OTROS DOS VALORES: ANCHO Y ALTO DEL TABLERO.
+
     constructor(title, height, width, sq_values, H_values, V_values) {
         this.title = title;
-        this.route = 'nono-solve';
         this.height = height;
         this.width = width;
         this.sq_values = sq_values;
@@ -10,22 +24,23 @@ class Nonon { //esta clase guarda todos los datos para mandar a un json
     }
 }
 
-var a = document.getElementById("hor").value
-var b = document.getElementById("vert").value
-var solve = false;
-var max;
-var comp;
-var solve_title;
-var BASTA = false;
+let a = document.getElementById("hor").value
+let b = document.getElementById("vert").value
+let solve = false;
+let max;
+let comp;
+let solve_title;
+let BASTA = false;
+let editor_mode = "off";
 
 function clearAll() { // función limpiar tabla
     document.getElementById("error").textContent = ""
     document.getElementById("nono").innerHTML = "";
-    document.getElementById("marcador-top").innerHTML = "";
-    document.getElementById("marcador-left").innerHTML = "";
 }
+
 function createSquares(valHor, valVert, id, val) { //funcion q arma la tabla
     let n=0; //n es el contador de cuadrados
+    boardEventListener(id)
     for(j=0;j<valVert;j++){
         let row = document.createElement("div");
         row.className = val + 'nonogrow' + j;
@@ -33,129 +48,81 @@ function createSquares(valHor, valVert, id, val) { //funcion q arma la tabla
         document.getElementById(id).appendChild(row) //creo las filas, cada una con una clase distinta.
         for(i=0;i<valHor;i++){
             let square = document.createElement("div");
-            square.className = 'nonog';
+            square.className = 'square_off';
             square.setAttribute("id", val + "square" + n);
             document.querySelector("." + row.className).appendChild(square) //dentro de las filas, cada cuadrado tiene un id distinto
-            escuchadorEvento(square.id)
+            squareEventListener(square.id)
             n+=1
         }
     }
-    if (solve === false) createMarkers(valHor, valVert, val, []);
 }
 
-function createMarkers(x, y, val, obj) {
-    for(i=0;i<x;i++) {
-        let marcadorTop = document.createElement("div");
-        marcadorTop.className = 'marcTop'
-        marcadorTop.setAttribute('id', val + "x_" + i);
-        if(solve === true) {
-            marcadorTop.innerHTML = obj.H_values[i]
-            document.querySelector("#mark-top-solve").appendChild(marcadorTop)
-        } else {
-        marcadorTop.innerHTML = "0";
-        document.querySelector("#marcador-top").appendChild(marcadorTop)
-        }
+
+// función click cambia de color al cuadrado
+
+// REEMPLAZAR ONCLICK POR FUNCIÓN QUE CUANDO UNO MANTENGA EL MOUSE APRETADO SOBRE EL CANVAS
+// PASE A SER MODO "EDICIÓN" Y DEPENDE DEL CUADRADO DONDE ESTÉ EL MOUSE DIBUJE O BORRE.
+
+function editingMode(toggle, child) {
+    if(toggle == 'draw')
+    {
+        console.log(toggle)
+        editor_mode = toggle;
+    } 
+    else if(toggle == "erase")
+    {
+        console.log(toggle)
+        editor_mode = toggle;
     }
-    for(j=0;j<y;j++) {
-        let marcadorLeft = document.createElement("div");
-        marcadorLeft.className = 'marcLeft'
-        marcadorLeft.setAttribute('id', val + "y_" + j);
-        if(solve === true) {
-            marcadorLeft.innerHTML = obj.V_values[j]
-            document.querySelector("#mark-left-solve").appendChild(marcadorLeft)
-        } else {
-            marcadorLeft.innerHTML = "0";
-            document.querySelector("#marcador-left").appendChild(marcadorLeft)    
-        }
+    else if(toggle == "off")
+    {
+        console.log(toggle)
+        editor_mode = toggle;
     }
 }
-// función click cambia de color al cuadrado
-function escuchadorEvento(ninio) {
-        const id = "#" + ninio
-        const element = document.querySelector(id);
-        let onoff = false;
-        element.addEventListener("click", () => {
-            if(BASTA === true) {}
-            else {
-                if (!onoff) {
-                    element.className = 'nonOn'
-                    onoff = true;
-                    if(solve === true) {
-                        escuchadorSolve(ninio)
-                    } else {
-                        escuchadorMark(ninio)
-                    }
+
+function squareEventListener(child) {
+        const element = document.querySelector("#" + child);
+        element.addEventListener("mousedown", () => {
+                if (element.className == 'square_on') 
+                {
+                    element.className = "square_off";
+                    editingMode("erase", child)
                 }
-                else {
-                    element.className = 'nonog'
-                    onoff = false;
-                    if(solve === true) {
-                        escuchadorSolve(ninio)
-                    } else {
-                        escuchadorMark(ninio)
-                    }
-                }
+                else 
+                {
+                    element.className = "square_on";
+                    editingMode("draw", child)
+                }});
+
+        element.addEventListener("mouseup", () => {
+            editingMode("off")
+        })
+
+        element.addEventListener("mouseenter", () => {
+            if(editor_mode == "draw"){
+                element.className = "square_on";
+            } else if (editor_mode == "erase"){
+                element.className = "square_off";
             }
         })
+    }
+
+function boardEventListener(id)
+{
+    const element = document.querySelector("#" + id);
+    element.addEventListener("mouseleave", () =>{
+        if(editor_mode != "off")
+        editingMode("off")
+    })
 }
 
-function escuchadorMark(idRaw) {
-    const id = parseInt(idRaw.replace("square", ""))
-    var x = parseInt(document.getElementById("hor").value)
-    var y = parseInt(document.getElementById("vert").value)
-    var max = x*y
-    let hLoc = id
-    let hVer = 0
-    while(hLoc>=x) {
-        hVer++
-        hLoc-=x
-    }
-    const hElement = document.querySelector('#x_'+hLoc)
-    const vElement = document.querySelector('#y_'+hVer)
-    let n = 0;
-    let accH = [];
-    let itt;
-    for(let q=hLoc;q<max;q+=x) {
-        if(document.querySelector('#square'+q).className === 'nonOn') {
-            n+=1
-            itt = true;
-        } else if (document.querySelector('#square'+q).className === 'nonog') {
-            if(n>0) {
-                accH.push(n); 
-                n=0
-                itt = false;
-            }
-        }
-    }
-    if(itt === true) accH.push(n);
-    hElement.innerHTML = accH.join(' ');
-    if(hElement.innerHTML === '') hElement.innerHTML = '0'
-    let n2=0;
-    let accV = []
-    let vLoc = x*hVer
-    let itt2;
-    for(let q=vLoc;q<vLoc+x;q++) {
-        if(document.querySelector('#square'+q).className === 'nonOn') {
-            n2+=1
-            itt2 = true;
-        } else if (document.querySelector('#square'+q).className === 'nonog') {
-            if(n2>0) {
-                accV.push(n2); 
-                n2=0
-                itt2 = false;
-            }
-        }
-    }
-    if (itt2 === true) accV.push(n2);
-    vElement.innerHTML = accV.join(',');
-    if(vElement.innerHTML === '') vElement.innerHTML = '0'
-}
-
-function escuchadorSolve() {
+function escuchadorSolve() 
+{
     let check;
     let correct = true;
     for(let i=0; i<max; i++) {
-        document.querySelector('#solve-square'+i).className === 'nonOn'? check = 1 : check = 0;
+        document.querySelector('#solve-square'+i).className === 'square_on'? check = 1 : check = 0;
         if(check != comp[i]) {
             correct = false; 
             break;
@@ -171,7 +138,7 @@ document.querySelector("#botonazo").onclick=function(){ //función de formulario
         document.getElementById("error").textContent = "This will not render more than 40 squares ヾ(〃^∇^)ﾉ"
     } else if(isNaN(a) || isNaN(b)) {
         document.getElementById("error").textContent = "Now try with a number (◕‿-)"
-    } else if(a < 1 || b < 1) {
+    } else if(a < 5 || b < 5) {
         document.getElementById("error").textContent = "Don't even try it ლ(╹ε╹ლ)"
     } else {
         clearAll();
@@ -186,7 +153,7 @@ document.querySelector("#inputN").onclick=function(){
     let height = b;
     let sq_values = [];
     for(let i=0;i<(a*b);i++) {
-        document.querySelector('#square'+i).className === 'nonOn'? sq_values.push('1') : sq_values.push('0')
+        document.querySelector('#square'+i).className === 'square_on'? sq_values.push('1') : sq_values.push('0')
     }  
     let H_values = [];
     for(let i=0; i<a;i++){
@@ -214,7 +181,6 @@ function createSolvable (nono) {
     comp = nono.sq_values
     solve_title = nono.title
     createSquares(parseInt(nono.width), parseInt(nono.height), nono.route, 'solve-')
-    createMarkers(parseInt(nono.width), parseInt(nono.height), 'solve-', nono)
 }
 
 function unbindClick() {
